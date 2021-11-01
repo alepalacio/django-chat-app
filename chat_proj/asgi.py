e80346e61c5440f7ad95b9963c08a190223bcc92
alepalacio <alejandropalacio24@gmail.com>
2021-10-31 21:08:30 -0300

    chat server working

--- chat_proj/asgi.py
+++ chat_proj/asgi.py
@@ -1,4 +1,29 @@
 """
+The next step is to point the root routing configuration at the chat.routing module. In mysite/asgi.py, import AuthMiddlewareStack, URLRouter, and chat.routing; and insert a 'websocket' key in the ProtocolTypeRouter list in the following format:
+"""
+
+# mysite/asgi.py
+import os
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+import chat_app.routing
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat_proj.settings")
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat_app.routing.websocket_urlpatterns
+        )
+    ),
+})
+
+
+
+"""
 ASGI config for chat_proj project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
@@ -19,15 +44,15 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 Let’s start by creating a root routing configuration for Channels. A Channels routing configuration is an ASGI application that is similar to a Django URLconf, in that it tells Channels what code to run when an HTTP request is received by the Channels server.
 """
 
-# mysite/asgi.py
-import os
+# # mysite/asgi.py
+# import os
 
-from channels.routing import ProtocolTypeRouter
-from django.core.asgi import get_asgi_application
+# from channels.routing import ProtocolTypeRouter
+# from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    # Just HTTP for now. (We can add other protocols later.)
-})
+# application = ProtocolTypeRouter({
+#     "http": get_asgi_application(),
+#     # Just HTTP for now. (We can add other protocols later.)
+# })
--- chat_proj/settings.py
+++ chat_proj/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,6 +75,15 @@ WSGI_APPLICATION = 'chat_proj.wsgi.application'
 # Channels
 ASGI_APPLICATION = 'chat_proj.asgi.application'
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
@@ -83,9 +92,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'TEST': {
+            'NAME': os.path.join(BASE_DIR, 'db_test.sqlite3')
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
